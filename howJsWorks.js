0ce750860500b4c4c84ae23b57cf1b07f5f75535
Shubham Mali <shubhammali31299@gmail.com>
2022-12-22 16:22:53 +0530

    done with compilation and execution theory

---
+++ howJsWorks.js
@@ -0,0 +1,16 @@
+//Compilation
+//Code Execution
+
+// why Compilation ?
+
+// How js code executes
+
+// What is global execution context ?
+// What is local execution context  ?
+// closures
+
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "Shubham";
+console.log(firstName);
